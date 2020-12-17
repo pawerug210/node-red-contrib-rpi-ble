b4e0d9e4af1e6a93c2ed96f86165663025f2033b
Pawel <pawelrugor26@wp.pl>
2020-12-17 19:58:24 +0100

    write node logging change

--- nodes/write/write.js
+++ nodes/write/write.js
@@ -3,10 +3,11 @@ const bleDevicesManager = ble_core.bleDevicesManager();
 
 module.exports = function (RED) {
     function WriteNode(config) {
-        console.debug('Creating WriteNode');
         RED.nodes.createNode(this, config);
-
         var node = this;
+
+        node.debug('Creating WriteNode');
+
         var characteristic = null;
 
         node.status({});
@@ -18,26 +19,26 @@ module.exports = function (RED) {
         }
 
         node.on('input', async function (msg) {
-            console.debug('WriteNode received input message: ' + JSON.stringify(msg));
+            node.debug('WriteNode received input message: ' + JSON.stringify(msg));
 
             if (characteristic) {
                 try {
                     await characteristic.writeValue(Buffer.from(msg.payload));
                     node.status({ fill: 'green', shape: 'ring', text: 'success' });
                 } catch (error) {
-                    console.error('Writing to characteristic error: ' + error);
+                    node.error('Writing to characteristic error: ' + error);
                     node.status({ fill: 'red', shape: 'ring', text: 'error' });
                 }
                 resetStatus(2);
             } else {
-                console.info('Characteristic not yet initialized, trying to retrieve it..');
+                node.log('Characteristic not yet initialized, trying to retrieve it..');
                 try {
                     characteristic = await bleDevicesManager.getCharacteristic(msg._deviceAddress,
                         msg._serviceUuid,
                         msg._characteristicUuid);
                 } catch (error) {
                     node.error('Cannot write to unknown characteristic, initialize it first');
-                    console.error('Writing to uninitialized characteristic returned error: ' + error);
+                    node.error('Writing to uninitialized characteristic returned error: ' + error);
                 }
             }
         })
@@ -45,10 +46,10 @@ module.exports = function (RED) {
         node.on('close', async function (removed, done) {
             if (removed) {
                 // This node has been disabled/deleted
-                console.debug('Node is closing as it got removed');
+                node.debug('Node is closing as it got removed');
             } else {
                 // This node is being restarted
-                console.debug('Node is closing as it going to be restarted');
+                node.debug('Node is closing as it going to be restarted');
             }
             done();
         })
