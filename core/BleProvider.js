591bca6865756f578acd383539468da948032642
Pawel <pawelrugor26@wp.pl>
2020-12-20 19:10:18 +0100

    logger in core classes

--- core/BleProvider.js
+++ core/BleProvider.js
@@ -1,7 +1,9 @@
+const logger = require('EasyLogger').logger;
+
 class BleProvider {
     constructor() {
         const node_ble = require('node-ble');
-        console.debug('Creating BleProvider');
+        logger.debug('Creating BleProvider');
         var createBluetooth = node_ble.createBluetooth();
         this.bluetooth = createBluetooth.bluetooth;
         this.destroy = createBluetooth.destroy;
@@ -12,18 +14,18 @@ class BleProvider {
         if (!this.adapter) {
             try {
                 if (adapter == null) {
-                    console.info('Creating default adapter');
+                    logger.info('Creating default adapter');
                     this.adapter = await this.bluetooth.defaultAdapter();
                 } else {
-                    console.info('Creating specific adapter: ' + adapter);
+                    logger.info('Creating specific adapter: ' + adapter);
                     this.adapter = await this.bluetooth.getAdapter(adapter);
                 }
             }
             catch (error) {
-                console.warn('Creating adapter error; ' + error);
+                logger.warn('Creating adapter error; ' + error);
             }
         } else {
-            console.debug('adapter already initialized');
+            logger.debug('adapter already initialized');
         }
         return this.adapter;
     }
@@ -31,19 +33,19 @@ class BleProvider {
     async startDiscovery() {
         //todo: timeout
         if (! (await this.adapter.isDiscovering())) {
-            console.info('Starting adapter discovery');
+            logger.info('Starting adapter discovery');
             await this.adapter.startDiscovery();
         } else {
-            console.debug('Trying to start adapter discovery but it was already discovering');
+            logger.debug('Trying to start adapter discovery but it was already discovering');
         }
     }
 
     async stopDiscovery() {
         if (await this.adapter.isDiscovering()) {
-            console.info('Stopping adapter discovery');
+            logger.info('Stopping adapter discovery');
             await this.adapter.stopDiscovery();
         } else {
-            console.debug('Trying to stop adapter discovery but it was not in discovering');
+            logger.debug('Trying to stop adapter discovery but it was not in discovering');
         }
     }
 
@@ -51,24 +53,24 @@ class BleProvider {
         address = address.toUpperCase();
         var device = null;
 
-        console.info('Requesting connection with device ' + address);
+        logger.info('Requesting connection with device ' + address);
         try {
             device = await this.adapter.waitDevice(address, timeout);
-            console.info('Device ' + address +' found, trying to connect...');           
+            logger.info('Device ' + address +' found, trying to connect...');           
             await device.connect();
         } catch (error) {
-            console.warn('Connection to device ' + address + ' error; ' + error);
+            logger.warn('Connection to device ' + address + ' error; ' + error);
             device = null;
         }
         return device;
     }
 
     destroy() {
-        console.debug('Requesting BT stream destruction');
+        logger.debug('Requesting BT stream destruction');
         try {
             this.destroy();
         } catch (error) {
-            console.warn('Destruction Error; ' + error);
+            logger.warn('Destruction Error; ' + error);
         }
         instance = null;
     }
@@ -78,7 +80,7 @@ class BleProvider {
 var instance = null;
 module.exports.getBleProvider = function() {
     if (!instance) {
-        console.log('Creating ble provider');
+        logger.log('Creating ble provider');
         instance = new BleProvider();
     }
     return instance;
